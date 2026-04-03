5b2083552a7a0f0aa279d799edcb2a3f970a0d82
Xavier <69229450+imalka90@users.noreply.github.com>
2026-04-04 01:51:14 +0530

    Update login.js

--- functions/login.js
+++ functions/login.js
@@ -1,1 +1,22 @@
+export async function onRequestPost(context) {
+  const { request, env } = context;
+  const { username, password } = await request.json();
 
+  // We use "sql_web_app" because that is the Variable Name from Step 2
+  const user = await env.sql_web_app.prepare(
+    "SELECT * FROM Users WHERE username = ? AND password = ?"
+  )
+  .bind(username, password)
+  .first();
+
+  if (user) {
+    return new Response(JSON.stringify({ success: true }), {
+      headers: { "Content-Type": "application/json" }
+    });
+  } else {
+    return new Response(JSON.stringify({ success: false }), {
+      status: 401,
+      headers: { "Content-Type": "application/json" }
+    });
+  }
+}
